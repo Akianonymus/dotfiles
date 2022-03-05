8098af5a832d6a79d2cb7ad3c30722922834847a
Akianonymus <anonymus.aki@gmail.com>
2022-03-05 20:26:44 +0530

    Add kde widget for conservation mode for laptops
    uses https://github.com/wsdfhjxc/do-it-yourself-bar

---
+++ src/misc/kde_diy_widgets/conservation_mode.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+node_path='/sys/bus/platform/drivers/ideapad_acpi/VPC2004:00/conservation_mode'
+
+id="$1"
+command="if [[ $(< ${node_path}) = 1 ]]; then  echo 0 > ${node_path} && kdialog --passivepopup '<br>Conservation Mode: Disabled' 1; else echo 1 > ${node_path} && kdialog --passivepopup '<br>Conservation Mode: Enabled' 1; fi"
+
+while true; do
+    status="Disabled" mode="A"
+    grep -q 1 "${node_path}" && status="Enabled" && mode="B"
+
+    data="| ${mode} || Conservation Mode: ${status} | $command |"
+
+    qdbus org.kde.plasma.doityourselfbar /id_"$id" \
+        org.kde.plasma.doityourselfbar.pass "$data"
+
+    sleep 1s
+done
