cd6a121b1cb0c43a0e62a6b3b2beddeba6aea068
Akianonymus <anonymus.aki@gmail.com>
2022-03-06 17:12:02 +0530

    Fix a derp in kde widget

--- src/misc/kde_diy_widgets/conservation_mode.sh
+++ src/misc/kde_diy_widgets/conservation_mode.sh
@@ -3,7 +3,7 @@
 node_path='/sys/bus/platform/drivers/ideapad_acpi/VPC2004:00/conservation_mode'
 
 id="$1"
-command="if [[ $(< ${node_path}) = 1 ]]; then  echo 0 > ${node_path} && kdialog --passivepopup '<br>Conservation Mode: Disabled' 1; else echo 1 > ${node_path} && kdialog --passivepopup '<br>Conservation Mode: Enabled' 1; fi"
+command="if [[ \$(< ${node_path}) = 1 ]]; then  echo 0 > ${node_path} && kdialog --passivepopup '<br>Conservation Mode: Disabled' 1; else echo 1 > ${node_path} && kdialog --passivepopup '<br>Conservation Mode: Enabled' 1; fi"
 
 while true; do
     status="Disabled" mode="A"
