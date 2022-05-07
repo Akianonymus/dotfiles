4f09f29d9f250a308b5d982574d45ba2c8a27f9f
Akianonymus <anonymus.aki@gmail.com>
2022-05-07 19:59:00 +0530

    neovim: trouble: use native font icons

--- src/.config/nvim/lua/custom/plugins/init.lua
+++ src/.config/nvim/lua/custom/plugins/init.lua
@@ -13,7 +13,7 @@ return {
       config = function()
          require("trouble").setup {
             auto_close = true, -- automatically close the list when you have no diagnostics
-            use_diagnostic_signs = false, -- enabling this will use the signs defined in your lsp client
+            use_diagnostic_signs = true, -- enabling this will use the signs defined in your lsp client
          }
       end,
    },
