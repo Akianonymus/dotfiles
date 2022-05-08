c2c426bc6aa87377907884a378c8aaaecb627992
Akianonymus <anonymus.aki@gmail.com>
2022-05-08 21:13:27 +0530

    neovim: Mapping changes

--- src/.config/nvim/lua/custom/mappings.lua
+++ src/.config/nvim/lua/custom/mappings.lua
@@ -126,23 +126,25 @@ end
 
 function M.misc()
    -- select all text in a buffer
-   map("n", "<C-a>", "gg0vG$")
-   map("x", "<C-a>", "gg0oG$")
+   map({ "n", "x" }, "<C-a>", "gg0vG$")
    -- save with c-s in all modes
-   map({ "n", "v", "i" }, "<C-s>", "<cmd>:update<cr>")
+   map({ "n", "x", "i" }, "<C-s>", "<cmd>:update<cr>")
    map("n", "<leader>fr", "<cmd>:Telescope resume<cr>")
    map("n", "<leader><leader>q", "<cmd>:qall<cr>")
    -- Reselect visual selection after indenting
    map("v", "<", "<gv")
    map("v", ">", ">gv")
-   -- do not select the new line on Y
+   -- do not select the new line on y
    map("n", "Y", "y$")
    map("x", "Y", "<Esc>y$gv")
    -- Keep matches center screen when cycling with n|N
    map("n", "n", "nzzzv")
    map("n", "N", "Nzzzv")
    -- swap ; with :
-   map("n", ";", ":")
+   map({ "n", "o", "x" }, ";", ":")
+   -- use H for start of line and L for end of line
+   map({ "n", "o", "x" }, "H", "0")
+   map({ "n", "o", "x" }, "L", "$")
 end
 
 return M
