55e7e0012fdbd8d14b1ad48007442aa38171982d
Akianonymus <anonymus.aki@gmail.com>
2022-07-23 15:48:18 +0530

    reformaat lua files

--- src/.config/nvim/lua/custom/init.lua
+++ src/.config/nvim/lua/custom/init.lua
@@ -6,13 +6,13 @@ require("custom.mappings").misc()
 -- require "custom.plugins"
 
 vim.defer_fn(function()
-   -- Create directory if missing: https://github.com/jghauser/mkdir.nvim
-   vim.cmd [[autocmd BufWritePre * lua require('custom.utils').create_dirs()]]
+  -- Create directory if missing: https://github.com/jghauser/mkdir.nvim
+  vim.cmd [[autocmd BufWritePre * lua require('custom.utils').create_dirs()]]
 
-   -- only enable treesitter folding if enabled
-   vim.cmd [[autocmd BufWinEnter * lua require('custom.utils').enable_folding()]]
+  -- only enable treesitter folding if enabled
+  vim.cmd [[autocmd BufWinEnter * lua require('custom.utils').enable_folding()]]
 
-   vim.cmd "hi! Folded guifg=#fff"
+  vim.cmd "hi! Folded guifg=#fff"
 
-   vim.cmd "silent! command Sudowrite lua require('custom.utils').sudo_write()"
+  vim.cmd "silent! command Sudowrite lua require('custom.utils').sudo_write()"
 end, 0)
--- src/.config/nvim/lua/custom/plugins/null-ls.lua
+++ src/.config/nvim/lua/custom/plugins/null-ls.lua
@@ -1,68 +1,68 @@
 local ok, null_ls = pcall(require, "null-ls")
 
 if not ok then
-   return
+  return
 end
 
 local b = null_ls.builtins
 local sources = {
 
-   -- c/c++
-   -- b.formatting.clang_format,
+  -- c/c++
+  -- b.formatting.clang_format,
 
-   -- python
-   b.formatting.black,
-   b.formatting.isort,
-   b.diagnostics.flake8,
+  -- python
+  b.formatting.black,
+  b.formatting.isort,
+  b.diagnostics.flake8,
 
-   -- rust
-   b.formatting.rustfmt,
+  -- rust
+  b.formatting.rustfmt,
 
-   -- JS html css stuff
-   b.formatting.prettierd.with {
-      filetypes = { "html", "json", "scss", "css", "javascript", "javascriptreact", "typescript" },
-   },
-   --    b.diagnostics.eslint.with {
-   --       command = "eslint_d",
-   --    },
+  -- JS html css stuff
+  b.formatting.prettierd.with {
+    filetypes = { "html", "json", "scss", "css", "javascript", "javascriptreact", "typescript" },
+  },
+  --    b.diagnostics.eslint.with {
+  --       command = "eslint_d",
+  --    },
 
-   -- Lua
-   b.formatting.stylua,
-   b.diagnostics.luacheck.with { extra_args = { "--global vim" } },
+  -- Lua
+  b.formatting.stylua,
+  b.diagnostics.luacheck.with { extra_args = { "--global vim" } },
 
-   -- Shell
-   b.formatting.shfmt,
-   b.diagnostics.shellcheck.with { diagnostics_format = "#{m} [#{c}]" },
+  -- Shell
+  b.formatting.shfmt,
+  b.diagnostics.shellcheck.with { diagnostics_format = "#{m} [#{c}]" },
 }
 
 local M = {}
 M.setup = function()
-   null_ls.setup {
-      sources = sources,
-      on_attach = function(client, bufnr)
-         if client.resolved_capabilities.document_formatting then
-            local map = vim.keymap.set
-            local buf_k = function(mo, k, c)
-               map(mo, k, c, { buffer = bufnr })
-            end
-            if client.resolved_capabilities.document_formatting then
-               buf_k("n", "<leader>fm", function()
-                  vim.lsp.buf.formatting()
-               end)
-               buf_k("v", "<leader>fm", function()
-                  vim.lsp.buf.range_formatting()
-               end)
-            end
+  null_ls.setup {
+    sources = sources,
+    on_attach = function(client, bufnr)
+      if client.resolved_capabilities.document_formatting then
+        local map = vim.keymap.set
+        local buf_k = function(mo, k, c)
+          map(mo, k, c, { buffer = bufnr })
+        end
+        if client.resolved_capabilities.document_formatting then
+          buf_k("n", "<leader>fm", function()
+            vim.lsp.buf.formatting()
+          end)
+          buf_k("v", "<leader>fm", function()
+            vim.lsp.buf.range_formatting()
+          end)
+        end
 
-            vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-               buffer = bufnr,
-               callback = function()
-                  vim.lsp.buf.formatting_seq_sync()
-               end, -- Or myvimfun
-            })
-         end
-      end,
-   }
+        vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+          buffer = bufnr,
+          callback = function()
+            vim.lsp.buf.formatting_seq_sync()
+          end, -- Or myvimfun
+        })
+      end
+    end,
+  }
 end
 
 return M
