f104be5f452ec9b1b9183de30f02bd786eb47e71
Akianonymus <anonymus.aki@gmail.com>
2022-08-23 15:14:53 +0530

    neovim: Disable swapfile
    annoying little shit

--- src/.config/nvim/lua/custom/init.lua
+++ src/.config/nvim/lua/custom/init.lua
@@ -2,6 +2,7 @@
 vim.opt.pumheight = 30
 vim.opt.tabstop = 4
 vim.opt.undofile = false
+vim.opt.swapfile = false
 
 vim.o.sessionoptions = "blank,buffers,curdir,localoptions,folds,help,tabpages,winsize,winpos,terminal"
 
