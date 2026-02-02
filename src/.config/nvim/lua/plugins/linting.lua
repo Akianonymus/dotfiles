25e6d3effc0f0a1be3e982533be35e88141455fd
akianonymus <anonymus.aki@gmail.com>
2026-02-02 13:57:57 +0530

    feat(lint): add eslint_d support for JS/TS files
    - Add eslint_d linter for javascript, javascriptreact, typescript,
      and typescriptreact filetypes
    - Configure autocommand to trigger linting on buffer write

--- src/.config/nvim/lua/plugins/linting.lua
+++ src/.config/nvim/lua/plugins/linting.lua
@@ -4,7 +4,20 @@ return {
     config = function()
       require("lint").linters_by_ft = {
         lua = { "luacheck" },
+        javascript = { "eslint_d" },
+        javascriptreact = { "eslint_d" },
+        typescript = { "eslint_d" },
+        typescriptreact = { "eslint_d" },
       }
+
+      local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+
+      vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+        group = lint_augroup,
+        callback = function()
+          require("lint").try_lint()
+        end,
+      })
     end,
   },
 }
